8d39c3e2b98f556e5ee1e8ccac7ea2f8e772256b
Mason-tom <mason.tom@stu.ocsb.ca>
2026-01-29 09:52:35 -0500

    When A pressed 1 cookie added when reached 10 doubles cookies

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mason
+ * Created on: Jan 2026
+ * This program makes a microbit version of cookie clicker
 */
 
-basic.showString('Hello, World!')
+let cookies = 0
+let cookiesPerClick = 1
+
+input.onButtonPressed(Button.A, function () {
+    cookies = cookies + cookiesPerClick
+    basic.showNumber(cookies)
+})
+
+input.onButtonPressed(Button.B, function () {
+    if (cookies >= 10) {
+        cookies = cookies - 10
+        cookiesPerClick = cookiesPerClick + 1
+        basic.showString("UP")
+    } else {
+        basic.showString("NO")
+    }
+})
+
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(cookies)
+})
+
+basic.showString("GO")
+
